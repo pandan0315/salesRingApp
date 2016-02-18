23aff54951ad7f5dda51c9938643a9599e2a6923
Khalid Saied <khalidomer88@gmail.com>
2016-02-18 01:56:17 +0100

    Updating the new post activity with better layout, and date picker

--- android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/NewPostActivity.java
+++ android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/NewPostActivity.java
@@ -1,24 +1,44 @@
 package com.example.zhehuan.saleapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import java.util.Calendar;
 
 public class NewPostActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView postPhoto;
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+    TextView dateTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
         postPhoto = (ImageView)findViewById(R.id.postPhoto);
         Glide.with(this).load(R.drawable.camera2).into(postPhoto);
+
+        dateTV = (TextView)findViewById(R.id.selectDateTV);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month, day + 1);
     }
 
 
@@ -28,11 +48,6 @@ public class NewPostActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void backToPreviousView(View view)
-    {
-        Intent intent = new Intent(NewPostActivity.this,MainActivity.class);
-        startActivity(intent);
-    }
 
 
 
@@ -50,6 +65,38 @@ public class NewPostActivity extends AppCompatActivity {
             Bitmap photo = (Bitmap) extra.get("data");
             postPhoto.setImageBitmap(photo);
         }
+    }
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateTV.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
     }
 
 }
