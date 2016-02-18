b8c609332e2b3029d76fae3e340b5c5ab8ca6cb6
Khalid Saied <khalidomer88@gmail.com>
2016-02-18 23:48:29 +0100

    Updating the profile activity

--- android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/UserProfileActivity.java
+++ android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/UserProfileActivity.java
@@ -2,6 +2,10 @@ package com.example.zhehuan.saleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        ImageView homeIV = (ImageView) findViewById(R.id.homeIB);
+        ImageView userProfileIV = (ImageView)findViewById(R.id.userProfileIB);
+        ImageView friendsIV = (ImageView) findViewById(R.id.friendsIB);
+        ImageView newPostIV = (ImageView)findViewById(R.id.newPostIB);
+        Glide.with(this).load(R.drawable.internet).into(homeIV);
+        Glide.with(this).load(R.drawable.social).into(userProfileIV);
+        Glide.with(this).load(R.drawable.editing).into(newPostIV);
+        Glide.with(this).load(R.drawable.people).into(friendsIV);
+
+
+        // temp configuration to be removed later
+        ImageView myProfile = (ImageView) findViewById(R.id.userProfilePhoto);
+        Glide.with(this).load("https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0b2/019/14e0f5c.jpg").into(myProfile);
+
     }
 }
