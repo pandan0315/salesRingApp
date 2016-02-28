a411ece618624c5c6cbe774e3c5637bf48a2e51f
Dan Pan <pandan0315@gmail.com>
2016-02-28 23:05:10 +0100

    Revert "change left activity String.ip_address"
    This reverts commit 368472c27238433701edccf807f0e543704761a7.

--- android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/FriendActivity.java
+++ android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/FriendActivity.java
@@ -57,15 +57,13 @@ public class FriendActivity extends AppCompatActivity {
         ImageView friendsIV = (ImageView) findViewById(R.id.friendsButt);
         ImageView newPostIV = (ImageView)findViewById(R.id.newPostButt);
         ImageView searchIcon = (ImageView) findViewById(R.id.searchIcon);
-       // ImageButton addIcon = (ImageButton)findViewById(R.id.addBtn);
-       ImageButton addIcon=(ImageButton)findViewById(R.id.addBtn);
-
+        ImageView deleteIcon = (ImageView) findViewById(R.id.deleteIB);
         Glide.with(this).load(R.drawable.internet).into(homeIV);
         Glide.with(this).load(R.drawable.social).into(userProfileIV);
         Glide.with(this).load(R.drawable.editing).into(newPostIV);
         Glide.with(this).load(R.drawable.people).into(friendsIV);
         Glide.with(this).load(R.drawable.searchicon).into(searchIcon);
-        Glide.with(this).load(R.drawable.social1).into(addIcon);
+        Glide.with(this).load(R.drawable.social1).into(deleteIcon);
 
        // friends = new ArrayList<FriendDetail>();
 
--- android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/PostsAdapter.java
+++ android_studioCode/SaleApp/app/src/main/java/com/example/zhehuan/saleapp/PostsAdapter.java
@@ -2,7 +2,6 @@ package com.example.zhehuan.saleapp;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,8 +19,6 @@ import com.bumptech.glide.signature.StringSignature;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
-import cz.msebera.android.httpclient.client.cache.Resource;
 
 import static com.example.zhehuan.saleapp.R.layout.friendsposts;
 
@@ -50,15 +47,15 @@ class PostsAdapter extends ArrayAdapter<SalePost> {
         //Glide.with(mainActivity).load(R.drawable.b).into(postIV);
 
        // Glide.with(mainActivity).load(R.drawable.poster).into(userProfilePhoto);
-        Glide.with(mainActivity).load("http://"+ mainActivity.getString(R.string.IP_address) +":8080/shares/image/"+posts.get(position).getPoster()+".jpeg").thumbnail(0.1f)
+        Glide.with(mainActivity).load("http://130.229.185.204:8080/shares/image/"+posts.get(position).getPoster()+".jpeg").thumbnail(0.1f)
                  .signature(new StringSignature(UUID.randomUUID().toString()))
                 .error(R.drawable.poster).into(userProfilePhoto);
 
         System.out.println(posts.get(position).getImageName());
 
-        Glide.with(mainActivity).load("http://"+ getContext().getString(R.string.IP_address) +":8080/shares/image/"+posts.get(position).getImageName()).into(postIV);
+        Glide.with(mainActivity).load("http://130.229.185.204:8080/shares/image/"+posts.get(position).getImageName()).into(postIV);
 
-                userNameTV.setText(posts.get(position).getPosterfullname());
+        userNameTV.setText(posts.get(position).getPosterfullname());
         dateTV.setText(posts.get(position).getPostDate());
         saleTV.setText(posts.get(position).getSaleValue()+"% OFF sale at  " +posts.get(position).getStore());
 
